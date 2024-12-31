b9e6f2c3443b5961f4c5a73d73146f34365fc1cb
박시현 <dkdi4224@naver.com>
2024-12-31 16:26:23 +0900

    feature: login 구현

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import SignUp from "../components/SignUp.vue";
+import Login from '../components/Login.vue';
 
 const routes = [
     {
         path: '/signup',
         name: 'Home',
         component: SignUp,
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: Login,
     },
 ];
 
